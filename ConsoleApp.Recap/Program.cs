a4b85fbd653c62032ffb4b91de2381527921588f
DAMILARE OBAFEMI <damimmanuel7@gmail.com>
2022-04-14 10:13:23 +0100

    Add project files.

---
+++ ConsoleApp.Recap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp.Recap
+{
+    public  class Program
+    {
+        static void Main()
+        {
+            PrintNumbers(1, 2, 3, 4,5);
+        }
+        public static void PrintNumbers(params int[] numbers)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ Library.Recap/Animal.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Library.Recap
+{
+    public class Animal
+    {
+    }
+}
